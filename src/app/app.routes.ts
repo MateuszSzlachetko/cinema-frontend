de1bb812a3f29b3de573ac621b8a642de870f4b6
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2024-05-25 14:35:17 +0200

    feat: add landing page component, style app background, dark theme and at some mock-ups for lp section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
