1d00de5870985cbe1bc020bf7cb491dcbf8c85cc
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2024-06-04 20:38:52 +0200

    feat: add carousel fallback and date button selection indicator

--- src/app/pages/screenings-page/screenings-page.component.ts
+++ src/app/pages/screenings-page/screenings-page.component.ts
@@ -1,5 +1,5 @@
 import {Component, inject} from '@angular/core';
-import {DatePipe, formatDate} from "@angular/common";
+import {DatePipe, formatDate, NgClass} from "@angular/common";
 import {ScreeningService} from "../../core/services/screening.service";
 import {ScreeningCardComponent} from "../../components/screening-card/screening-card.component";
 import {ScreeningInterface, ScreeningsInterface} from "../../core/interfaces/screening.interface";
@@ -8,6 +8,7 @@ import {ActivatedRoute, Params, Router} from "@angular/router";
 import {MoviesCarouselComponent} from "../../components/movies-carousel/movies-carousel.component";
 
 const dayInMilliseconds: number = 1000 * 60 * 60 * 24;
+const numOfDaysInAdvance: number = 7;
 
 @Component({
   selector: 'app-screenings-page',
@@ -15,31 +16,38 @@ const dayInMilliseconds: number = 1000 * 60 * 60 * 24;
   imports: [
     DatePipe,
     ScreeningCardComponent,
-    MoviesCarouselComponent
+    MoviesCarouselComponent,
+    NgClass
   ],
   templateUrl: './screenings-page.component.html',
   styleUrl: './screenings-page.component.scss'
 })
 export class ScreeningsPageComponent {
   screeningService: ScreeningService = inject(ScreeningService);
-  dates = Array.from({length: 7},
+  dates = Array.from({length: numOfDaysInAdvance},
     (_, i) => new Date(Date.now() + i * dayInMilliseconds));
   moviesWithScreenings: Map<MovieInterface, ScreeningInterface[]> = new Map();
   activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   router: Router = inject(Router);
+  buttonSelected: boolean[] = Array.from({length: numOfDaysInAdvance}, _ => false);
 
 
   constructor() {
     this.activatedRoute.data.subscribe(({screenings$}) => {
-      screenings$.subscribe((response: ScreeningsInterface) => {
-        this.updateScreenings(response)
-      })
+      screenings$.subscribe((response: ScreeningsInterface) => this.updateScreenings(response))
+    });
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      const date = params["date"] || Date.now();
+      this.updateSelectedButton(new Date(date));
     })
+
   }
 
   getScreenings(index: number) {
     const date = this.dates[index];
     this.updateUrl(date)
+    this.updateSelectedButton(date);
     this.screeningService.getScreeningsByDate(date).subscribe(
       (response: ScreeningsInterface) => this.updateScreenings(response)
     );
@@ -67,4 +75,14 @@ export class ScreeningsPageComponent {
     ).then(r => r);
   }
 
+  updateSelectedButton(date: Date) {
+    const compareDates = (date1: Date, date2: Date) => {
+      date1.setHours(0, 0, 0, 0)
+      date2.setHours(0, 0, 0, 0)
+      return date1.getTime() === date2.getTime()
+    }
+    const index = this.dates.findIndex(d => compareDates(d, date));
+    this.buttonSelected = Array.from({length: numOfDaysInAdvance}, _ => false);
+    this.buttonSelected[index] = true;
+  }
 }
